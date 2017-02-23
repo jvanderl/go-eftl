397c4502757fa584e043b7ee9b844b5b70d2f800
TIBCO User <tibco@tibco4.demo.local>
2017-02-23 18:42:25 +0100

    first usable version

--- eftl.go
+++ eftl.go
@@ -1,4 +1,5 @@
 package eftl
+package eftl
 
 import (
 	"github.com/gorilla/websocket"
@@ -13,33 +14,47 @@ const STATE_OPEN int = 1
 const STATE_CLOSING int = 2
 const STATE_CLOSED int = 3
 
+const EFTL_VERSION = "3.1.0   V7"
+const EFTL_WS_PROTOCOL = "v1.eftl.tibco.com"
+const OP_HEARTBEAT = 0
+const OP_LOGIN = 1
+const OP_WELCOME = 2
+const OP_SUBSCRIBE = 3
+const OP_SUBSCRIBED = 4
+const OP_UNSUBSCRIBE = 5
+const OP_UNSUBSCRIBED = 6
+const OP_EVENT = 7
+const OP_MESSAGE = 8
+const OP_ACK = 9
+const OP_ERROR = 10
+const OP_DISCONNECT = 11
+const OP_GOODBYE = 12
+const SUBSCRIPTION_TYPE = ".s."
 
 
-var log = logging.MustGetLogger("lib-go-eftl")
-
-type eftlConnection struct {
-    connectOptions string
-    accessPointURL string
-    webSocket websocket.Conn
-    state int
-    clientId string
-    reconnectToken string
-    timeout int
-    heartbeat int
-    maxMessageSize int
-    lastMessage int
-    timeoutCheck int
-    subscriptions map[string]string //todo struct
-    sequenceCounter int
-    subscriptionCounter int
-    reconnectCounter int
-    reconnectAttempts int
-    reconnectTimer int
-    isReconnecting bool
-    isOpen bool
-    qos bool
-    sendList map[string]string // todo struct
-    lastSequenceNumber int
+type Connection struct {
+    ConnectOptions string
+    AccessPointURL string
+    WebSocket *websocket.Conn
+    State int
+    ClientID string
+    ReconnectToken string
+    Timeout int
+    Heartbeat int
+    MaxMessageSize int
+    LastMessage int
+    TimeoutCheck int
+    Subscriptions []Subscription //todo struct
+    SequenceCounter int
+    SubscriptionCounter int
+    ReconnectCounter int
+    ReconnectAttempts int
+    ReconnectTimer int
+    IsReconnecting bool
+    IsOpen bool
+    QoS bool
+    SendList map[string]string // todo struct
+    LastSequenceNumber int
 }
 
 type eftlLoginRequest struct {
@@ -57,22 +72,27 @@ type eftlLoginResponse struct {
 	IDToken string `json:"id_token"`
 	Timeout int `json:"timeout"`
 	Heartbeat int `json:"heartbeat"`
-	MaxSize int `json:"max_size"`
+	MaxMessageSize int `json:"max_size"`
 	QoS string `json:"_qos"`
 }
 
-
 type eftlSubscriptionRequest struct {
 	Operator int  `json:"op"`
-	ClientID string `json:"id"`	
-	Matcher string `json:"matcher"`
+	SubscriptionID string `json:"id"`	
+	Matcher string `json:"matcher,omitempty"`
+	Durable string `json:"durable,omitempty"`
 }
-
 type eftlSubscriptionResponse struct {
 	Operator int  `json:"op"`
 	SubscriptionID string `json:"id"`	
 }
 
+type eftlUnsubscriptionResponse struct {
+	Operator int  `json:"op"`
+	SubscriptionID string `json:"id"`	
+	Error int `json:"err"`
+	Reason string `json:"reason"`
+}
 
 type eftlBody struct {
 	Destination string `json:"_dest"`
@@ -90,7 +110,7 @@ type eftlInboundMessage struct {
 type eftlOutboundMessage struct {
 	Operator int  `json:"op"`
 	Body eftlBody `json:"body"`
-	Sequence int `json:"seq"`
+	Sequence int `json:"seq",omitempty`
 } 
 
 type eftlAckMsg struct {
@@ -98,34 +118,72 @@ type eftlAckMsg struct {
 	Sequence int `json:"seq"`
 }
 
-type eftlSubsctiption struct {
-	SubscriptionID string
-
+type Subscription struct {
+	ID string `json:"subscription_id"`
+	Matcher string `json:"matcher"`
+	Durable string `json:"durable"`
+	Pending bool `json:"pending"`
 }
 
-func Login (conn eftlConnection, user string, password string)  (err error){
+func Connect(server string, channel string, options string) (conn Connection, err error) {
+
+	wsURL := url.URL{Scheme: "ws", Host: server, Path: channel}
+
+	// init eftlConnection
+    conn.ConnectOptions = options
+    conn.AccessPointURL = wsURL.String()
+    conn.State = STATE_OPENING
+    conn.Timeout = 600000
+    conn.Heartbeat = 240000
+    conn.MaxMessageSize = 0
+    conn.LastMessage = 0
+    conn.SequenceCounter = 0
+    conn.SubscriptionCounter = 0
+    conn.ReconnectCounter = 0
+    conn.ReconnectAttempts = 5
+    conn.IsReconnecting = false
+    conn.IsOpen = false
+    conn.QoS = true
+    conn.SendList = nil // todo struct
+    conn.LastSequenceNumber = 0
+
+    // Connect to eftl Server
+	conn.WebSocket, _, err = websocket.DefaultDialer.Dial(conn.AccessPointURL, nil)
+	if err != nil {
+		return conn, err
+	}
+	return conn, nil
+}
+
+
+func (conn *Connection) Login (user string, password string)  (err error){
 	
-	loginMessage := eftlLoginRequest{1, "js", "3.1.0   V7", user, password, map[string]string{"_qos": "true"}}
+	loginMessage := eftlLoginRequest{OP_LOGIN, "js", EFTL_VERSION, user, password, map[string]string{"_qos": "true"}}
 
 	loginb, err := json.Marshal(loginMessage)
 	if err != nil {
 		return err
 	}
 	
-	err = conn.webSocket.WriteMessage(websocket.TextMessage, loginb)
+	err = conn.WebSocket.WriteMessage(websocket.TextMessage, loginb)
 	if err != nil {
 		return err
 	}
 
-	msg, op := GetMessage (conn.webSocket)
+	msg, op := conn.GetMessage()
 	switch op {
-		case 2 : { // Login response
-    		res := new(eftlLoginResponse)
+		case OP_WELCOME : { 
+    		res := eftlLoginResponse{}
 		    if err := json.Unmarshal(msg, &res); err != nil {
 				return err
 			}
-			conn.clientId := res.ClientID
-			conn.reconnectToken := res.IDToken 
+			conn.ClientID = res.ClientID
+			conn.ReconnectToken = res.IDToken
+			conn.State = STATE_OPEN
+			conn.Timeout = res.Timeout
+			conn.Heartbeat = res.Heartbeat
+			conn.MaxMessageSize = res.MaxMessageSize
+			conn.QoS, _ = strconv.ParseBool(res.QoS)
 			return nil
 		}
 		default: {
@@ -135,114 +193,53 @@ func Login (conn eftlConnection, user string, password string)  (err error){
 	return err
 }
 
-/*func Login (conn websocket.Conn, user string, password string) (clientid string, idtoken string, err error){
-	
-	loginMessage := eftlLoginRequest{1, "js", "3.1.0   V7", user, password, map[string]string{"_qos": "true"}}
+func (conn *Connection) Subscribe (matcher string, durable string) (subscriptionid string, err error){
 
-	loginb, err := json.Marshal(loginMessage)
-	if err != nil {
-		return "", "", err
-	}
-	
-	err = conn.WriteMessage(websocket.TextMessage, loginb)
-	if err != nil {
-		return "", "", err
-	}
+	subID := conn.ClientID + SUBSCRIPTION_TYPE + strconv.Itoa(conn.nextSubscriptionSequence()) + ""
+    var subscription = Subscription{};
+    subscription.ID = subID;
+   	subscription.Matcher = matcher;
+   	subscription.Durable = durable
+    subscription.Pending = true;
 
-	msg, op := GetMessage (conn)
-	switch op {
-		case 2 : { // Login response
-    		res := new(eftlLoginResponse)
-		    if err := json.Unmarshal(msg, &res); err != nil {
-				return "", "", err
-			}
-			return res.ClientID, res.IDToken, nil
-		}
-		default: {
-		}
-	}
-	err = errors.New("No login response received")
-	return "", "", err
-}
-*/
-
-func Connect(server string, channel string, options *string) (conn *eftlConnection, err error) {
-
-	//wsURL := url.URL{Scheme: "ws", Host: server, Path: channel}
-	conn = eftConnection{}
-    conn.connectOptions = options
-    conn.accessPointURL = url.URL{Scheme: "ws", Host: server, Path: channel}
-//    conn.webSocket = nil
-    conn.state = STATE_OPENING
-    conn.clientId = nil
-    conn.reconnectToken = nil
-    conn.timeout = 600000
-    conn.heartbeat = 240000
-    conn.maxMessageSize = 0
-    conn.lastMessage = 0
-    conn.timeoutCheck = nil
-    conn.subscriptions = nil
-    conn.sequenceCounter = 0
-    conn.subscriptionCounter = 0
-    conn.reconnectCounter = 0
-    conn.reconnectAttempts = 5
-    conn.reconnectTimer = nil
-    conn.isReconnecting = false
-    conn.isOpen = false
-    conn.qos = true
-    conn.sendList = nil // todo struct
-    conn.lastSequenceNumber = 0
-	conn.webSocket, _, err = websocket.DefaultDialer.Dial(wsURL.String(), nil)
-	if err != nil {
-		return nil, err
-	}
-	return conn, nil
-}
-
-/*func Connect(server string, channel string) (conn *websocket.Conn, err error) {
-
-	wsURL := url.URL{Scheme: "ws", Host: server, Path: channel}
-
-	conn, _, err = websocket.DefaultDialer.Dial(wsURL.String(), nil)
-	if err != nil {
-		return nil, err
-	}
-	return conn, nil
-}*/
-
-func Subscribe (conn websocket.Conn, clientid string, index int, destination string) (subscriptionid string, err error){
-
-	subscriptionMessage := eftlSubscriptionRequest{3, clientid + ".s." + strconv.Itoa(index) + "", "{\"_dest\":\"" + destination + "\"}"}
-
+	subscriptionMessage := eftlSubscriptionRequest{OP_SUBSCRIBE, subID, matcher, subscription.Durable}
 	subscrb, err := json.Marshal(subscriptionMessage)
 	if err != nil {
 		return "", err
 	}
-	
-	err = conn.WriteMessage(websocket.TextMessage, subscrb)
+	err = conn.WebSocket.WriteMessage(websocket.TextMessage, subscrb)
 	if err != nil {
 		return "", err
-	} 
+	}
 
-	msg, op := GetMessage (conn)
+	msg, op := conn.GetMessage()
 	switch op {
-		case 4 : { // subscription response
+		case OP_SUBSCRIBED : { // subscription response
     		res := eftlSubscriptionResponse{}
 		    if err := json.Unmarshal(msg, &res); err != nil {
 				return "", err
 			}
+			conn.Subscriptions =  append(conn.Subscriptions, subscription)
 			return res.SubscriptionID, nil
+		}
+		case OP_UNSUBSCRIBED : {
+    		res := eftlUnsubscriptionResponse{}
+		    if err := json.Unmarshal(msg, &res); err != nil {
+				return "", err
+			}    		
+    		err = errors.New(res.Reason)
+    		return "", err
 		}
 		default: {
 		}
 	}
-	err = errors.New("No subscription response received")
+	err = errors.New("Other message received: " + convert(msg))
 	return "", err
 }
 
-func GetMessage (conn websocket.Conn) (message []byte, operator int){
+func (conn *Connection) GetMessage () (message []byte, operator int){
 	for {
-		messageType, p, err := conn.ReadMessage()
+		messageType, p, err := conn.WebSocket.ReadMessage()
 		if err == nil {
 			switch messageType {
 		    	case websocket.TextMessage : {
@@ -268,12 +265,36 @@ func GetMessage (conn websocket.Conn) (message []byte, operator int){
 	}
 }
 
-func SendMessage (conn websocket.Conn, message string, destination string)
+func (conn *Connection) SendMessage (message string, destination string) (err error) {
+	var sequence int = conn.nextSequence()
 	res := eftlOutboundMessage{}
-	res.Operator := 8
-	res.Body.Destination := destination
-	res.Body.Text := message
-
+	res.Operator = OP_MESSAGE
+	res.Body.Destination = destination
+	res.Body.Text = message
+	res.Body.Number = sequence
+	if conn.QoS {
+		res.Sequence = sequence
+	}
+		
+	msg, err := json.Marshal(res)
+	if err != nil {
+		return err
+	}
+	if (conn.MaxMessageSize > 0 && len(msg) > conn.MaxMessageSize) {
+		err = errors.New("Message exceeds maximum message size of " + string(conn.MaxMessageSize))
+		return err
+	}
+	if conn.State == STATE_OPEN {
+		err = conn.WebSocket.WriteMessage(websocket.TextMessage, msg)
+		if err != nil {
+			return err
+		} 
+	}  else {
+		err = errors.New("Connection not open")
+		return err
+	}
+	return nil
+}
 
 func MessageDetails(message []byte) (text string, destination string, err error) {
 	res := eftlInboundMessage{}
@@ -281,6 +302,16 @@ func MessageDetails(message []byte) (text string, destination string, err error)
 		return "","", err
 	}
 	return res.Body.Text, res.Body.Destination, nil
+}
+
+func (conn *Connection) nextSequence() int {
+	conn.SequenceCounter += 1
+	return conn.SequenceCounter
+}
+
+func (conn *Connection) nextSubscriptionSequence() int {
+	conn.SubscriptionCounter += 1
+	return conn.SubscriptionCounter
 }
 
 func convert(b []byte) string {
